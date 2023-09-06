9bd90ae86a4bc5ee14db41dba9fb4319587bf5f3
Fabian Stiven Valencia Cordoba <fabian.cordoba@correounivalle.edu.co>
2023-09-06 11:16:48 -0500

    Portfolio sesion 3

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -6,9 +6,9 @@ import { WoodenFence } from "./World/WoodenFence";
 const Experience = () => {
     return (
         <>
+            <OrbitControls makeDefault />
             <ambientLight intensity={0.5} />
             <directionalLight position={[10, 10, 5]} intensity={2} />
-            <OrbitControls makeDefault/>
             <Bee />
             <Tree position={[-2, -2, 2]} scale={0.04} />
             <WoodenFence position={[0, -2, 0]} />
